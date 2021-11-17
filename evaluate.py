5f3b88ae4695ed14863a63f914de372c8b1ac09d
Sophie <xiaoyuanguo.ucas@gmail.com>
2021-11-17 15:58:18 -0500

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -14,11 +14,41 @@ def get_fpr_tpr_auc(Y_label, Y_preds):
     return fpr, tpr, aucscore
 
 
+def cvae_evaluate(embnet, recon_loss test_dataloader, device):
+    logger = logging.getLogger()
+    logger.info("----------- CVAE evaluating------------")
+    Targets = []
+    anomaly_score = []
+
+    with torch.set_grad_enabled(False):    
+        for idx, inputs in enumerate(test_dataloader):
+        
+            images, targets = inputs
+            images = images.to(device)
+
+            for i in range(0, images.shape[0]):
+                recon_x, mu, logvar, mu2, logvar2 = embnet(images[i].unsqueeze(0))
+                cvae_loss = recon_loss(recon_x, images[i].unsqueeze(0), mu, logvar, mu2, logvar2)
+
+                if not np.isnan(cvae_loss.item()) and not np.isinf(cvae_loss.item()):
+                    anomaly_score.append(cvae_loss.item())
+                    Targets.append(targets[i].detach().cpu().numpy())
+            
+    Y_label = np.array(np.vstack(Targets).squeeze(1),dtype=int).tolist() 
+    Y_preds = []
+    for s in anomaly_score:
+        Y_preds.append((s-np.min(np.array(anomaly_score)))/(np.max(np.array(anomaly_score))-np.min(np.array(anomaly_score)))
+    aucscore = None
+    fpr, tpr, aucscore = get_fpr_tpr_auc(Y_label, Y_preds) 
+    return fpr, tpr, aucscore  
+
+
 def cvad_evaluate(embnet, cls_model, recon_loss, cls_loss, test_dataloader, device):
     logger = logging.getLogger()
     logger.info("----------- CVAD evaluating------------")
     Targets = []
-    anomaly_score = []
+    anomaly_score1 = []
+    anomaly_score2 = []
 
     with torch.set_grad_enabled(False):    
         for idx, inputs in enumerate(test_dataloader):
@@ -32,13 +62,14 @@ def cvad_evaluate(embnet, cls_model, recon_loss, cls_loss, test_dataloader, devi
                 cvae_loss = recon_loss(recon_x, images[i].unsqueeze(0), mu, logvar, mu2, logvar2)
 
                 if not np.isnan(cvae_loss.item()+outputs.detach().cpu().numpy()[0][0]) and not np.isinf(cvae_loss.item()+outputs.detach().cpu().numpy()[0][0]):
-                    anomaly_score.append(cvae_loss.item()+outputs.detach().cpu().numpy()[0][0])
+                    anomaly_score1.append([cvae_loss.item()])
+                    anomaly_score2.append([outputs.detach().cpu().numpy()[0][0]])
                     Targets.append(targets[i].detach().cpu().numpy())
             
     Y_label = np.array(np.vstack(Targets).squeeze(1),dtype=int).tolist() 
     Y_preds = []
-    for s in anomaly_score:
-        Y_preds.append((s-np.min(np.array(anomaly_score)))/(np.max(np.array(anomaly_score))-np.min(np.array(anomaly_score))))
+    for s1, s2 in zip(anomaly_score1, anomaly_score2):
+        Y_preds.append(0.5*((s1-np.min(np.array(anomaly_score1)))/(np.max(np.array(anomaly_score1))-np.min(np.array(anomaly_score1))) + s2))
     aucscore = None
     fpr, tpr, aucscore = get_fpr_tpr_auc(Y_label, Y_preds) 
     return fpr, tpr, aucscore  
