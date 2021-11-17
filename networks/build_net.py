48e19ae05102c2cf87850e8b1743c76fb0f9bd0a
Sophie <xiaoyuanguo.ucas@gmail.com>
2021-11-17 16:04:55 -0500

    Update build_net.py

--- networks/build_net.py
+++ networks/build_net.py
@@ -1,5 +1,6 @@
 import logging
 from .CVAE import *
+from .CVAE32 import *
 from .Discriminator import *
 
 
@@ -7,9 +8,14 @@ def build_CVAD_net(dataset_name, net_name, capacity, channel):
     
     logger = logging.getLogger()
     logger.info("Build CVAD embnet for {}".format(dataset_name))
-    assert dataset_name in ['rsna', 'siim']
+    assert dataset_name in ['cifar10', 'siim']
     
-    netG = CVAE(capacity,channel)
-    netD = Discriminator(capacity, channel)
+    if dataset_name == "cifar10":
+        netG = VAE_all32(capacity,channel)
+        netD = Discriminator32(capacity, channel)  
+        
+    else:
+        netG = CVAE(capacity,channel)
+        netD = Discriminator(capacity, channel)
 
     return netG, netD
